c88f602c3ac46605c6b2c0cabef1e26d250c54a7
abhijit-space101 <abhijit.shinde@thoughtworks.com>
2026-08-22 22:15:43 +0530

    service bus

---
+++ ServiceBus.Receiver/Program.cs
@@ -0,0 +1,52 @@
+﻿
+using Azure.Messaging.ServiceBus;
+
+const string serviceBusConnectionString = "Endpoint=sb://az-service-bus-learn.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=X7nBp0kWuWM3H+PRTdRGMCIeuZYcDfAx2+ASbNQG/aU=";
+
+//const string queueName = "az-queue-learn";
+
+const string topicName = "az-topic-learn";
+const string sub1Name = "sub-1";
+ServiceBusClient client;
+ServiceBusProcessor processor = default!;
+
+async Task MessageHandler(ProcessMessageEventArgs processMessageEventArgs)
+{
+    string body = processMessageEventArgs.Message.Body.ToString();
+    Console.WriteLine($"{body} - Subscription: {sub1Name}");
+    Console.WriteLine($"{body}");
+    await processMessageEventArgs.CompleteMessageAsync(processMessageEventArgs.Message);
+}
+
+Task ErrorHandler(ProcessErrorEventArgs processMessageEventArgs)
+{
+    Console.WriteLine(processMessageEventArgs.Exception.ToString());
+    return Task.CompletedTask;
+}
+
+client = new ServiceBusClient(serviceBusConnectionString);
+//processor = client.CreateProcessor(queueName, new ServiceBusProcessorOptions());
+processor = client.CreateProcessor(topicName, sub1Name, new ServiceBusProcessorOptions());
+
+try
+{
+    processor.ProcessMessageAsync += MessageHandler;
+    processor.ProcessErrorAsync += ErrorHandler;
+
+    await processor.StartProcessingAsync();
+    Console.WriteLine("Press any key to end the processing");
+    Console.ReadKey();
+
+    Console.WriteLine("\nStopping the receiver...");
+    await processor.StopProcessingAsync();
+    Console.WriteLine("Stopped receiving messages");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Exception: {ex.Message}");
+}
+finally
+{
+    await processor.DisposeAsync();
+    await client.DisposeAsync();
+}
---
+++ ServiceBus.Sender/Program.cs
@@ -0,0 +1,38 @@
+﻿using Azure.Messaging.ServiceBus;
+
+const string serviceBusConnectionString = "Endpoint=sb://az-service-bus-learn.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=X7nBp0kWuWM3H+PRTdRGMCIeuZYcDfAx2+ASbNQG/aU=";
+
+//const string queueName = "az-queue-learn";
+const string topicName = "az-topic-learn";
+const int maxNumberofMessages = 3;
+
+ServiceBusClient client;
+ServiceBusSender sender;
+
+client = new ServiceBusClient(serviceBusConnectionString);
+sender = client.CreateSender(topicName);
+//sender = client.CreateSender(queueName);
+
+using ServiceBusMessageBatch batch = await sender.CreateMessageBatchAsync();
+for (int i = 1; i <= maxNumberofMessages; i++)
+{
+    if (!batch.TryAddMessage(new ServiceBusMessage($"This a message - {i}")))
+    {
+        Console.WriteLine($"Message - {i} was not added to the batch");
+    }
+}
+
+try
+{
+    await sender.SendMessagesAsync(batch);
+    Console.WriteLine("Messages Sent");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Exception: {ex.Message}");
+}
+finally
+{
+    await sender.DisposeAsync();
+    await client.DisposeAsync();
+}
